2e82f8c56a387c278ac95c63e73624f0885ca70e
Ruslan Popesku <ruslanpopesku95@gmail.com>
2025-11-06 10:43:52 +0200

    Update AppDelegate.swift

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Sentry
 
 
 @UIApplicationMain
@@ -17,31 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        SentrySDK.start { options in
-            options.dsn = "https://a39afceb2e4b7ef1b3210cb6ec480077@o1271632.ingest.us.sentry.io/4510285189545984"
-
-            // Adds IP for users.
-            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
-            options.sendDefaultPii = true
-
-            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-            // We recommend adjusting this value in production.
-            options.tracesSampleRate = 0.5
-
-            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
-            options.configureProfiling = {
-                $0.sessionSampleRate = 0.5 // We recommend adjusting this value in production.
-                $0.lifecycle = .trace
-            }
-
-            // Uncomment the following lines to add more data to your events
-            // options.attachScreenshot = true // This adds a screenshot to the error events
-            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
-            
-            // Enable experimental logging features
-            options.experimental.enableLogs = true
-        }
-
     // Override point for customization after application launch.
     return true
   }
